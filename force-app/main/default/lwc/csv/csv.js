88d54071744c929fc6d334244f8451494147b6b4
1cristo <2020229@utsh.edu.mx>
2024-02-21 12:54:24 -0600

    upload csv

--- force-app/main/default/lwc/csv/csv.js
+++ force-app/main/default/lwc/csv/csv.js
@@ -1,14 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class ImportarDatosDesdeCSV extends LightningElement {
-    importDate;
-    filePath;
-    importMessage;
-    importMessageClass;
+    @track importDate;
+    @track filePath;
+    @track importMessage;
+    @track importMessageClass;
+    @track importedData; // Nueva propiedad para almacenar los datos importados
+    @track columns;
 
     handleFileChange(event) {
         const selectedFile = event.target.files[0];
         this.filePath = selectedFile.name;
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            const csvData = this.csvJSON(reader.result);
+            const headers = this.extractHeaders(csvData);
+
+            this.columns = headers.map(header => ({
+                label: header,
+                fieldName: header,
+                type: 'text'
+            }));
+
+            // Asignar los datos al componente
+            this.importedData = csvData;
+        };
+
+        reader.readAsText(selectedFile);
     }
 
     handleDateChange(event) {
@@ -16,9 +35,8 @@ export default class ImportarDatosDesdeCSV extends LightningElement {
     }
 
     importData() {
-        // Lógica para importar datos desde el archivo CSV
-
-        // Simulación de éxito o error (puedes personalizar esto según tus necesidades)
+        // Lógica para importar datos desde el archivo CSV (puedes implementar según tus necesidades)
+        // En este ejemplo, simplemente se muestra un mensaje de éxito o error
         const success = true; // o false
         if (success) {
             this.importMessage = 'Datos importados con éxito.';
@@ -27,5 +45,31 @@ export default class ImportarDatosDesdeCSV extends LightningElement {
             this.importMessage = 'Error al importar datos. Verifica el archivo CSV.';
             this.importMessageClass = 'error-message';
         }
+    }
+
+    csvJSON(csv) {
+        // Lógica para convertir el CSV a formato JSON
+        const lines = csv.split('\n');
+        const result = [];
+        const headers = lines[0].split(',');
+
+        for (let i = 1; i < lines.length; i++) {
+            const obj = {};
+            const currentLine = lines[i].split(',');
+
+            for (let j = 0; j < headers.length; j++) {
+                obj[headers[j]] = currentLine[j];
+            }
+
+            result.push(obj);
+        }
+
+        return result;
+    }
+
+    extractHeaders(csvData) {
+        // Lógica para obtener los encabezados del CSV
+        // En este ejemplo, simplemente se toman las claves del primer objeto en el array
+        return Object.keys(csvData[0]);
     }
 }
